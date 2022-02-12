cb043822351d9527aeaf01a2c184df54be22caf7
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2022-02-12 11:55:49 -0800

    Invoke flit from pip instead of invoking directly

--- tooling/setup.ps1
+++ tooling/setup.ps1
@@ -1,8 +1,8 @@
 copier -rf 8b707c6
 py -3.10 -m venv --clear --upgrade .venv
 .venv/Scripts/activate
-pip uninstall -y boilerdata
 pip install -U pip  # throws [WinError 5], but works
 pip install -U setuptools wheel  # must be done separately from above
 pip install -r tooling/requirements_dev.txt
-flit install -s
+pip uninstall -y boilerdata
+pip install -e .
