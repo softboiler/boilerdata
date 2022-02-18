3e5b4af98ec9992d6f465ab8b0a338956f0557a8
blakeNaccarato <blake.naccarato@gmail.com>
2022-02-18 10:15:40 -0800

    Bump template

--- tooling/setup.ps1
+++ tooling/setup.ps1
@@ -1,8 +1,4 @@
-copier -f -r 71a2e1d
-py -3.10 -m venv --clear --upgrade .venv
-.venv/Scripts/activate
-pip install -U pip  # throws [WinError 5], but works
-pip install -U setuptools wheel  # must be done separately from above
-pip install -r tooling/requirements_dev.txt
-pip uninstall -y boilerdata
-pip install -e .
+copier -f -r a578b9f
+Remove-Item .venv -Recurse -ErrorAction SilentlyContinue
+py -3.10 -m venv .venv
+. tooling/update.ps1
