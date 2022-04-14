2c2c74e0c57cc7c74fa37cef0773f25316749ff6
blakeNaccarato <blake.naccarato@gmail.com>
2022-04-14 13:27:29 -0700

    Don't rglob when loading configs

--- src/boilerdata/configs.py
+++ src/boilerdata/configs.py
@@ -8,7 +8,7 @@ import boilerdata
 DEFAULT_CONFIG_FILENAME = "defaults.toml"
 USER_CONFIG_FILENAME = "boilerdata.toml"
 default_path = Path(boilerdata.__path__[0]) / DEFAULT_CONFIG_FILENAME  # type: ignore
-user_path = next(Path().rglob(USER_CONFIG_FILENAME), Path(USER_CONFIG_FILENAME))
+user_path = Path(USER_CONFIG_FILENAME)
 raw_config = Dynaconf(settings_files=[default_path, user_path])
 
 
