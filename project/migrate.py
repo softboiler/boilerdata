190d2a22505ace2aa0c4bae4f128fe7f1890224e
blakeNaccarato <blake.naccarato@gmail.com>
2022-05-18 12:54:54 -0700

    Test the first migration

--- project/migrate.py
+++ project/migrate.py
@@ -2,91 +2,116 @@
 
 from bisect import insort
 
-from pydantic import BaseModel
+from pydantic import BaseModel, DirectoryPath
 
-from boilerdata.utils import dump_model, load_config
-from models.project import Project
+from boilerdata.utils import StrPath, dump_model, load_config
 
 TRIALS_PATH = "project/config/trials.yaml"
-
-
-class PartialTrial(BaseModel):
-    date: str
-    rod: str
-    coupon: str
-    sample: str
-    group: str
-    monotonic: bool
-    joint: str
-    comment: str
-
-
-class PartialTrials(BaseModel):
-    trials: list[PartialTrial]
 
 
 def main():
     pass
 
 
-def get_partial_trials():
+def migrate_1(project_path: StrPath, trials_path: StrPath):
+    """Migration 1: Partially populate a config file containing trials info.
 
-    raise Exception("You have already run this migration.")
+    This migration was informed by the original trials folder structure. The "Boiling
+    Curves" subfolder had mostly well-behaved, mostly monotonic results. The "Test Runs"
+    subfolder had unbehaved trials, with less distinct folder names.
 
-    config = load_config("project/config/project.yaml", Project)
-    good_trials = list((config.trials / "Boiling Curves").iterdir())
-    okay_trials = list((config.trials / "Test Runs").iterdir())
-    trials = []
-    for trial in good_trials + okay_trials:
+    Parameters
+    ----------
+    project_path: StrPath
+        The path to `project.yaml`.
+    trials_path: StrPath
+        The path to `trials.yaml`.
+    """
 
-        match trial.name.split(" "):
-            case [date, "Copper", spec] if trial in good_trials:
-                comment = ""
-                rod, coupon, sample, group = match_sample_spec(spec)
-            case [date, *rest]:
-                comment = " ".join(rest)
-                rod = coupon = sample = group = ""
+    def main():
+        config = load_config(project_path, Project)
+        good_trials = list((config.trials / "Boiling Curves").iterdir())
+        okay_trials = list((config.trials / "Test Runs").iterdir())
+        trials = []
+        for trial in good_trials + okay_trials:
+
+            match trial.name.split(" "):
+                case [date, "Copper", spec] if trial in good_trials:
+                    comment = ""
+                    rod, coupon, sample, group = match_sample_spec(spec)
+                case [date, *rest]:
+                    comment = " ".join(rest)
+                    rod = coupon = sample = group = ""
+                case _:
+                    raise ValueError(f'Couldn\'t parse "{trial.name}"')
+
+            monotonic = trial in good_trials
+            joint = ""
+            date = "20" + date.replace(".", "-")
+
+            trial = Trial(
+                date=date,
+                rod=rod,
+                coupon=coupon,
+                sample=sample,
+                group=group,
+                monotonic=monotonic,
+                joint=joint,
+                comment=comment,
+            )
+
+            insort(trials, trial, key=lambda t: t.date)
+
+        dump_model(trials_path, Trials(trials=trials))
+
+    def match_sample_spec(spec):
+        """Match the dash-delimited sample specification of various formats."""
+
+        match spec.split("-"):
+            case [rod, coupon, sample, _]:  # Like X-A6-B3-1
+                pass
+            case [rod, coupon, _]:  # Like Y-A7-2
+                sample = "NA"
+            case [coupon, _]:  # Like A4-12
+                rod = "W"
+                sample = "NA"
             case _:
-                raise ValueError(f'Couldn\'t parse "{trial.name}"')
+                raise ValueError(f'Couldn\'t parse "{spec}"')
 
-        monotonic = trial in good_trials
-        joint = ""
-        date = "20" + date.replace(".", "-")
+        if "B" in sample:
+            group = "porous"
+        else:
+            group = "control"
 
-        trial = PartialTrial(
-            date=date,
-            rod=rod,
-            coupon=coupon,
-            sample=sample,
-            group=group,
-            monotonic=monotonic,
-            joint=joint,
-            comment=comment,
-        )
+        return rod, coupon, sample, group
 
-        insort(trials, trial, key=lambda t: t.date)
+    class Project(BaseModel):
+        """Project configuration prior to Migration 1."""
 
-    dump_model(TRIALS_PATH, PartialTrials(trials=trials))
+        trials: DirectoryPath
 
+    class Trial(BaseModel):
+        """Configuration for a single trial after Migration 1.
 
-def match_sample_spec(spec):
-    match spec.split("-"):
-        case [rod, coupon, sample, _]:  # Like X-A6-B3-1
-            pass
-        case [rod, coupon, _]:  # Like Y-A7-2
-            sample = "NA"
-        case [coupon, _]:  # Like A4-12
-            rod = "W"
-            sample = "NA"
-        case _:
-            raise ValueError(f'Couldn\'t parse "{spec}"')
+        This configuration is less strict because we aren't informing their values from
+        specified Enums.
+        """
 
-    if "B" in sample:
-        group = "porous"
-    else:
-        group = "control"
+        date: str
+        rod: str
+        coupon: str
+        sample: str
+        group: str
+        monotonic: bool
+        joint: str
+        comment: str
 
-    return rod, coupon, sample, group
+    class Trials(BaseModel):
+        """Top-level configuration for a list of trials after Migration 1."""
+
+        trials: list[Trial]
+
+    main()
 
 
 if __name__ == "__main__":
--- project/test_project.py
+++ project/test_project.py
@@ -1,33 +1,37 @@
-from contextlib import contextmanager
-from os import chdir, getcwd
 from pathlib import Path
-from shutil import copytree
 
-import pandas as pd
-import pipeline
-from pytest import mark as m
+import yaml
 
-DATA = Path("tests/data")
-RESULT = Path("fitted.csv")
+from migrate import migrate_1
 
 
-@contextmanager
-def working_directory(path: Path):
-    original_working_directory = getcwd()
-    try:
-        chdir(path)
-        yield
-    finally:
-        chdir(original_working_directory)
+def test_migrate_1(tmp_path):
+    base = Path("project/tests/migrate/migrate_1")
+    migrate_1(base / "project.yaml", tmp_path / "trials.yaml")
+    result = yaml.safe_load((tmp_path / "trials.yaml").read_text())
+    expected = yaml.safe_load(Path(base / "trials.yaml").read_text())
+    assert result == expected
 
 
-@m.skip
-def test_run(tmp_path):
-    """Ensure the same result is coming out of the pipeline as before."""
-    test_data = tmp_path / "data"
-    copytree(DATA, test_data)
-    with working_directory(test_data):
-        pipeline.run()
-    result = pd.read_csv(test_data / RESULT)
-    expected = pd.read_csv(DATA / RESULT)
-    pd.testing.assert_frame_equal(result, expected)
+# DATA = Path("tests/data")
+# RESULT = Path("fitted.csv")
+
+# @contextmanager
+# def working_directory(path: Path):
+#     original_working_directory = getcwd()
+#     try:
+#         chdir(path)
+#         yield
+#     finally:
+#         chdir(original_working_directory)
+
+# @m.skip
+# def test_run(tmp_path):
+#     """Ensure the same result is coming out of the pipeline as before."""
+#     test_data = tmp_path / "data"
+#     copytree(DATA, test_data)
+#     with working_directory(test_data):
+#         pipeline.run()
+#     result = pd.read_csv(test_data / RESULT)
+#     expected = pd.read_csv(DATA / RESULT)
+#     pd.testing.assert_frame_equal(result, expected)
