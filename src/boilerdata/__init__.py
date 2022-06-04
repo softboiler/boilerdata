35dfc2a2d55586a3267f1bcd9c68163539d56497
blakeNaccarato <blake.naccarato@gmail.com>
2022-06-03 19:15:13 -0700

    Revert "Temporarily stop using `rich` since it breaks Jupyter debug"
    This reverts commit 7de2bba1b27c2f94c9b86a91b74abe5c93cfe1e0.

--- src/boilerdata/__init__.py
+++ src/boilerdata/__init__.py
@@ -2,8 +2,7 @@
 
 __version__ = "0.0.0"
 
-# # Breaks Jupyter debug
-# from rich import pretty, traceback
-#
-# pretty.install()
-# traceback.install()
+from rich import pretty, traceback
+
+pretty.install()
+traceback.install()
