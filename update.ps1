40df3025d7366c04f48c162d5de3d87b1735202d
blakeNaccarato <blake.naccarato@gmail.com>
2022-07-06 13:01:18 -0700

    Install propshop from main to avoid issues with deps

--- update.ps1
+++ update.ps1
@@ -4,3 +4,4 @@ pip install -U setuptools wheel  # must be done separately from above
 pip install -U -r .tools/requirements/requirements_dev.txt
 pip uninstall -y boilerdata
 pip install -e .
+pip install -U -r requirements_dynamic.txt
