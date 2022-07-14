d5d02b5349b83906c455b1287b07e3d7362ed266
blakeNaccarato <blake.naccarato@gmail.com>
2022-07-14 08:38:22 -0700

    Are these requirements being installed?

--- .tools/scripts/cicd_setup.sh
+++ .tools/scripts/cicd_setup.sh
@@ -1,4 +1,6 @@
 pip install -U pip setuptools wheel
 pip install -r .tools/requirements/requirements_cicd.txt
 pip install .
+echo "I AM ABOUT TO INSTALL DYNAMIC REQUIREMENTS"
 pip install -r requirements_dynamic.txt
+echo "I HAVE FINISHED INSTALLING DYNAMIC REQUIREMENTS"
