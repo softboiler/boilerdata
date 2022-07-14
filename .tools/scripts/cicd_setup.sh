f6c2f9431b2661bcc19e1cf9aaf33a189328fd74
blakeNaccarato <blake.naccarato@gmail.com>
2022-07-14 08:45:36 -0700

    Make propshop public and revert this echo

--- .tools/scripts/cicd_setup.sh
+++ .tools/scripts/cicd_setup.sh
@@ -1,6 +1,4 @@
 pip install -U pip setuptools wheel
 pip install -r .tools/requirements/requirements_cicd.txt
 pip install .
-echo "I AM ABOUT TO INSTALL DYNAMIC REQUIREMENTS"
 pip install -r requirements_dynamic.txt
-echo "I HAVE FINISHED INSTALLING DYNAMIC REQUIREMENTS"
