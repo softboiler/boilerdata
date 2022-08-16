28ced339f7df5406cc049c80dd7add159b241d64
blakeNaccarato <blake.naccarato@gmail.com>
2022-08-15 17:00:13 -0700

    Bump template

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,7 @@
-copier -f -r 38bebe4
-py -3.10 -m venv .venv --clear --upgrade-deps
+$GLOBAL_PYTHON = 'py -3.10'
+try { Invoke-Expression $("$GLOBAL_PYTHON --version") } catch [System.Management.Automation.CommandNotFoundException] { $GLOBAL_PYTHON = 'python3.10' }
+Remove-Item -ErrorAction SilentlyContinue -Recurse -Force .venv
+Invoke-Expression "$GLOBAL_PYTHON -m venv .venv --upgrade-deps"
 . ./update.ps1
+pre-commit install
+pre-commit
