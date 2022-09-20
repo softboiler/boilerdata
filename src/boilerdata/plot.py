dfeacd9dac5ad1e705e1f51204fe668395df0081
blakeNaccarato <blake.naccarato@gmail.com>
2022-09-20 12:13:44 -0700

    Plot properly

--- src/boilerdata/plot.py
+++ src/boilerdata/plot.py
@@ -4,4 +4,6 @@ from boilerdata.origin import get_path, open_originlab
 
 with open_originlab("data/plotter/results.opju"):
     gp = op.find_graph("lit_")
-    gp.save_fig(get_path("data/plots", mkdirs=True), width=800)
+    fig = gp.save_fig(get_path("data/plots", mkdirs=True), type="png", width=800)
+    if not fig:
+        raise RuntimeError("Failed to save figure.")
