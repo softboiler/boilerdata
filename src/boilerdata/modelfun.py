726cc8ce869cf85f4d758fe2d6e61cd5616cc04a
blakeNaccarato <blake.naccarato@gmail.com>
2022-10-18 17:21:15 -0700

    Take model parameters prior to ODE integration

--- src/boilerdata/modelfun.py
+++ src/boilerdata/modelfun.py
@@ -1,31 +1,12 @@
-from sympy import Derivative, Eq, Function, dsolve, lambdify, linsolve
-from sympy.abc import a, b, c, f, g, x
+from sympy import Derivative, Function, dsolve, lambdify
+from sympy.abc import a, b, c, x
 
 y = Function("y")
-ode_solution = dsolve(
-    eq=Derivative(y(x), x) + f * x + g,
+_model = dsolve(
+    eq=Derivative(y(x), x) + a * x + b,
     fun=y(x),
     ics={y(0): c},
-)
-_model = (
-    linsolve(
-        # The system of equations we want to solve
-        (
-            ode_solution,  # `.args[0].args[0]` selects this equation after solving
-            Eq(a, -f / 2),
-            Eq(b, -g),
-        ),
-        # The variables we want to eliminate
-        (
-            y(x),
-            f,
-            g,
-        ),
-    )
-    .args[0]
-    .args[0]
-)
-
+).rhs  # type: ignore
 _slope = _model.diff(x)  # type: ignore
 
 model = lambdify((x, a, b, c), _model)
