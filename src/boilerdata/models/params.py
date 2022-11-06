837fc06adca13a5d81e1e9183a6fe5eeb4683bd0
blakeNaccarato <blake.naccarato@gmail.com>
2022-11-05 18:19:53 -0700

    Use `Field()` for parameters

--- src/boilerdata/models/params.py
+++ src/boilerdata/models/params.py
@@ -19,6 +19,18 @@ class Params(MyBaseModel):
         description="The number of records over which to average in a given trial.",
     )
 
+    # Reason: pydantic: use_enum_values
+    model_params: list[A] = Field(
+        default=[
+            A.T_s,
+            A.q_s,
+            A.k,
+            A.h_a,
+            A.h_w,
+        ],
+        description="Parameters that can vary in the model. Some will be fixed.",
+    )
+
     model_inputs: dict[str, float] = Field(
         default=dict(
             r=0.0047625,  # (m)
@@ -27,32 +39,26 @@ class Params(MyBaseModel):
             x_s=0,  # (m)
             x_wa=0.0381,  # (m)
         ),
-        description="The inputs to the model to be fitted.",
+        description="Inputs to the symbolic model float evaluation stage.",
     )
-
-    # Reason: pydantic: use_enum_values
-    model_params: list[A] = [
-        A.T_s,
-        A.q_s,
-        A.k,
-        A.h_a,
-        A.h_w,
-    ]  # type: ignore
 
     # ! MODEL BOUNDS
 
     # Reason: pydantic: use_enum_values
-    model_bounds: dict[A, tuple[bound, bound]] = {
-        A.T_s: (95, "inf"),  # (C) T_s
-        A.q_s: (0, "inf"),  # (W/m^2) q_s
-        A.k: (350, 450),  # (W/m-K) k
-        A.h_a: (0, "inf"),  # (W/m^2-K) h_a
-        A.h_w: (0, "inf"),  # (W/m^2-K) h_w
-    }  # type: ignore
+    model_bounds: dict[A, tuple[bound, bound]] = Field(
+        default={
+            A.T_s: (95, "inf"),  # (C) T_s
+            A.q_s: (0, "inf"),  # (W/m^2) q_s
+            A.k: (350, 450),  # (W/m-K) k
+            A.h_a: (0, "inf"),  # (W/m^2-K) h_a
+            A.h_w: (0, "inf"),  # (W/m^2-K) h_w
+        },
+        description="Bounds for the model parameters. Not used if parameter is fixed.",
+    )
 
     @validator("model_bounds", always=True)
     def validate_model_bounds(cls, model_bounds) -> dict[A, tuple[float, float]]:
-        """Substitute inf for np.inf"""
+        """Substitute inf for np.inf."""
         for param, b in model_bounds.items():
             b0 = -np.inf if isinstance(b[0], str) and "inf" in b[0] else b[0]
             b1 = np.inf if isinstance(b[0], str) and "inf" in b[1] else b[1]
@@ -61,31 +67,37 @@ class Params(MyBaseModel):
 
     # ! FIXED PARAMS
 
-    fixed_params: list[A] = [
-        A.k,
-        A.h_w,
-    ]  # type: ignore
+    fixed_params: list[A] = Field(
+        default=[
+            A.k,
+            A.h_w,
+        ],
+        description="Parameters to fix. Evaluated before fitting, overridable in code.",
+    )
 
     @validator("fixed_params", always=True, each_item=True)
     def validate_each_fixed_param(cls, param, values):
-        """Substitute inf for np.inf"""
+        """Check that the fixed parameter is one of the model parameters."""
         if param in values["model_params"]:
             return param
         raise ValueError(f"Fixed parameter {param} not in model parameters")
 
     # ! INITIAL VALUES
 
-    initial_values: dict[A, float] = {
-        A.T_s: 95,  # (C) T_s
-        A.q_s: 0,  # (W/m^2) q_s
-        A.k: 400,  # (W/m-K) k
-        A.h_a: 0,  # (W/m^2-K) h_a
-        A.h_w: 0,  # (W/m^2-K) h_w
-    }  # type: ignore
+    initial_values: dict[A, float] = Field(
+        default={
+            A.T_s: 95,  # (C) T_s
+            A.q_s: 0,  # (W/m^2) q_s
+            A.k: 400,  # (W/m-K) k
+            A.h_a: 0,  # (W/m^2-K) h_a
+            A.h_w: 0,  # (W/m^2-K) h_w
+        },
+        description="Initial guess for free parameters, constant value otherwise.",
+    )
 
     @validator("initial_values", always=True)
     def validate_initial_values(cls, model_inputs) -> dict[str, float]:
-        """Substitute near zero instead of zero to avoid division by zero."""
+        """Avoid division by zero in select parameters."""
         eps = float(np.finfo(float).eps)
         params_to_check = (A.h_a, A.h_w)
         return {
