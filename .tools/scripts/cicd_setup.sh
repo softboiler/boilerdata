f4f5c808df323f8ccfe134b973da0c60c5c663a2
blakeNaccarato <blake.naccarato@gmail.com>
2022-11-07 09:34:36 -0800

    Generalize `pip` upgrading to support both Windows and Ubuntu runners

--- .tools/scripts/cicd_setup.sh
+++ .tools/scripts/cicd_setup.sh
@@ -1,4 +1,5 @@
-pip install -U pip setuptools wheel
+python -m pip install -U pip
+pip install -U setuptools wheel
 pip install -r .tools/requirements/requirements_cicd.txt
 pip install .
 pip install -r requirements_dynamic.txt
