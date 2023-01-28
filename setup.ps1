4a86b235fed79b403f03499d955551594b35e3b8
blakeNaccarato <blake.naccarato@gmail.com>
2023-01-27 17:18:41 -0800

    Resolve dependency issue with `propshop` and `boilerdata`.

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,7 @@
+if ($Env:VIRTUAL_ENV) { deactivate }
 $GLOBAL_PYTHON = 'py -3.10'
 try { Invoke-Expression $("$GLOBAL_PYTHON --version") } catch [System.Management.Automation.CommandNotFoundException] { $GLOBAL_PYTHON = 'python3.10' }
-Remove-Item -ErrorAction SilentlyContinue -Recurse -Force .venv
+Remove-Item -Recurse -Force .venv
 Invoke-Expression "$GLOBAL_PYTHON -m venv .venv --upgrade-deps"
 . ./update.ps1
 pre-commit install --install-hooks
