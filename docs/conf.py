1a26f4dfc2ccf5ddeb5771a7b0c1eaf97180af7f
blakeNaccarato <blake.naccarato@gmail.com>
2023-04-13 12:40:00 -0700

    Prepare to release

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,11 @@
 from datetime import date
 
-from myst_parser import __version__
-
-project = "Project"
-copyright = f"{date.today().year}, Blake Naccarato"  # noqa: A001
-version = __version__
+project = ""
+html_title = "Contents"
+copyright = f"{date.today().year}, Blake Naccarato, Kwang Jin Kim"  # noqa: A001
+version = "0.0.0"
 master_doc = "index"
 language = "en"
-extensions = ["myst_parser", "sphinx_design", "sphinxcontrib.mermaid"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_book_theme"
+extensions = ["myst_parser", "sphinx_design", "sphinxcontrib.mermaid"]
